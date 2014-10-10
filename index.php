68d563bf3951815856f294e8dd650b2cce97790e
Irene Brown <naaabrown@gmail.com>
2014-10-10 17:36:28 +0000

    added picture and blog

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Astral by HTML5 UP</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400" rel="stylesheet" />
+		<script src="js/jquery.min.js"></script>
+		<script src="js/config.js"></script>
+		<script src="js/skel.min.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel-noscript.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-desktop.css" />
+			<link rel="stylesheet" href="css/noscript.css" />
+		</noscript>
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><link rel="stylesheet" href="css/ie8.css" /><![endif]-->
+	</head>
+	<body class="homepage">
+
+		<!-- Wrapper-->
+			<div id="wrapper">
+				
+				<!-- Nav -->
+					<nav id="nav">
+						<a href="#me" class="fa fa-home active"><span>Home</span></a>
+						<a href="#work" class="fa fa-pencil-square-o"><span>Blog</span></a>
+						<a href="http://twitter.com/naaabrown" class="fa fa-twitter"><span>Twitter</span></a>
+					</nav>
+
+				<!-- Main -->
+					<div id="main">
+						
+												<!-- Me -->
+							<article id="me" class="panel">
+								<header>
+									<h1>Brown Irene</h1>
+									<span class="byline">Software Developer</span>
+								</header>
+								<a href="#work" class="jumplink pic">
+									<span class="jumplink arrow fa fa-chevron-right"><span>See my work</span></span>
+									<img src="/images/rene.jpg"  alt="300px" height="100px" />
+								</a>
+							</article>
+
+						<!-- blog --> 
+							<article id="work" class="panel">
+								<header>
+									<h2>FiME Blog</h2>
+								</header>
+								
+								<header>
+									
+								</header>
+								<p>
+									I am a young lady with 13years of working experience 6 of which has been in the IT field.
+I have always been dreaming of being an entrepreneur but had not yet gotten the courage to do so until last April when I finally decided to quit my job and prepare myself for this dream.
+I had planned to enter into Networking, Website & mobile App development and was thinking of where to go for this training. Upon speaking to one of my IT mentors, he introduced me to Mobile Web Ghana and gave me the contact of the Director (Florence).
+At my first call, Florence mentioned this planned program (FiME) to me and asked if I was interested.
+Though interested, I still pursued a 2week refresher program with Mobile Web Ghana.
+I applied for the FiME program, went through the as called for an interview, and pretty enjoying the program so far.
+The program started with an introduction to HTML and JavaScript. We continued with Mobile Value Added Services.
+By the end of the second week I was able to develop a mobile App named “To Do List” on my mobile phone using phoneGap and thanks to Selorm Banybah who made the session so interesting for us.
+Though I missed the UI/UX session since I was indisposed, the training material was so clear and self explanatory I could gather some idea on it.
+We are currently enjoying Saviour Kirk Agbenyegah who is taking us through Google Cloud Platform using Github.
+</br>
+To attain success, one would have to be committed to crossing all the hurdles that come our way.
+I must confess the timing of the training has not been favorable for me though, however I’m committed to getting the best out of it and to break the ice to seeing a dream come true.
+The only major problem is about internet connectivity which mostly slowed us down during the training. 
+Florence (Director), we are grateful for this idea to empower and train more females in entrepreneurship and mobile apps development. You are a GENIUS!
+Adams (Project Coordinator) has always been ready for us be it questions, support etc…
+</br>
+FiME has been an eye-opener and the preparation stage of a dream come true. 
+Big ups to MOBILE WEB GHANA!!!.
+ 
+									
+								</p>
+			
+
+					</div>
+		
+				<!-- Footer -->
+					<div id="footer">
+						<ul class="links">
+							<li>&copy; Irene Brown</li>
+							
+						</ul>
+					</div>
+		
+			</div>
+
+	</body>
+</html>
